324d82ab8968c90ba7a0ac194e04d82b6bddb1d9
Silent Crew <silentcrewtechnology@gmail.com>
2025-04-02 12:29:23 +0300

    Добавил проект заново

---
+++ Sources/SCDSAccessibilityIds/AuthAccessibilityIDs/AuthAccessibilityIDs.swift
@@ -0,0 +1,38 @@
+//
+//  AuthAccessibilityIDs.swift
+//
+//
+//  Created by Омельченко Юлия on 09.09.2024.
+//
+
+public class AuthAccessibilityIDs {
+    /// Экран выбора способа входа по карте/счету или логину
+    public class SegmentsScreen {
+        public static let id = "auth_segments_screen"
+        public static let tableView = "auth_segments_screen_table_view"
+        public static let alert = "auth_segments_screen_alert"
+    }
+    
+    /// Экран ввода смс кода
+    public class EnterCodeFromSmsScreen {
+        public static let id = "auth_enter_sms_code_screen_table_view"
+        public static let subtitleView = "auth_enter_sms_code_subtitle_view"
+        public static let subtitleLabel = "auth_enter_sms_code_subtitle_label"
+    }
+    
+    /// Экран создания пин-кода
+    public class CreatePinScreen {
+        public static let id = "auth_create_pin_screen_table_view"
+        public static let title = "auth_create_pin_title"
+        public static let alert = "auth_create_pin_screen_alert"
+    }
+    
+    /// Экран ввода пин-кода
+    public class EnterPinScreen {
+        public static let id = "auth_enter_pin_screen_table_view"
+        public static let alert = "auth_enter_pin_screen_alert"
+        public static let headerView = "auth_enter_pin_screen_header_view"
+        public static let dayLabel = "auth_enter_pin_screen_day_label"
+        public static let nameLabel = "auth_enter_pin_screen_name_label"
+    }
+}
---
+++ Sources/SCDSAccessibilityIds/CurrenciesAccessibilityIDs/CurrenciesAccessibilityIDs.swift
@@ -0,0 +1,23 @@
+//
+//  CurrenciesAccessibilityIDs.swift
+//
+//
+//  Created by Омельченко Юлия on 21.06.2024.
+//
+
+public class CurrenciesAccessibilityIDs {
+    /// Главный экран
+    public class QuotationListScreen {
+        public static let id = "quotation_list_table_view"
+        public static let activityIndicator = "quotation_list_activity_indicator"
+        
+        public static let headerInfoRow = "header_info_row"
+        public static let headerInfoRowLabel = "header_info_row_label"
+        
+        public static let infoTextRow = "info_text_row"
+        public static let infoTextRowLabel = "info_text_row_label"
+        
+        public static let infoAlert = "quotation_list_info_alert"
+        public static let infoAlertOkButton = "quotation_list_info_alert_ok_button"
+    }
+}
