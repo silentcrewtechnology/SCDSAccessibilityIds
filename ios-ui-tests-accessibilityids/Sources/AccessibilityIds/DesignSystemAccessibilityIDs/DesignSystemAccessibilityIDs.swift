01ca1ae83cdcbb8905a708d07a6d9b90adc2b528
silentcrewtechnology <silentcrewtechnology@gmail.com>
2025-04-02 11:59:15 +0300

    Добавил библиотеку в репозиторий

---
+++ ios-ui-tests-accessibilityids/Sources/AccessibilityIds/DesignSystemAccessibilityIDs/DesignSystemAccessibilityIDs.swift
@@ -0,0 +1,161 @@
+//
+//  DesignSystemAccessibilityIDs.swift
+//
+//
+//  Created by user on 24.06.2024.
+//
+
+public class DesignSystemAccessibilityIDs {
+    public class CardView {
+        public static let id = "card_view"
+        public static let backgroundImageView = "card_view_background_image"
+        public static let paymentSystemImageView = "card_view_payment_system_image"
+        public static let centerImageView = "card_view_center_image"
+        public static let cardNumberLabel = "card_view_card_number_label"
+        public static let bandView = "card_view_band_view"
+        public static let containerView = "card_view_container_view"
+    }
+    
+    public class CheckboxView {
+        public static let id = "checkbox_view"
+        public static let checkView = "checkbox_view_check_view"
+    }
+    
+    public class LabelView {
+        public static let id = "label_view"
+        public static let labelView = "label_view_label_view"
+        public static let container = "label_view_container"
+    }
+    
+    public class SegmentItemView {
+        public static let id = "segment_item_view_"
+        public static let containerView = "segment_item_container_view"
+        public static let label = "segment_item_view_label"
+    }
+    
+    public class SegmentControl {
+        public static let id = "segment_control_view"
+        public static let containerView = "segment_control_container_view"
+        public static let segmentStack = "segment_control_stack"
+    }
+    
+    public class InputView {
+        public static let id = "input_view"
+        public static let label = "input_view_label"
+        public static let hint = "input_view_hint"
+        public static let textField = "input_view_text_field"
+        public static let verticalStack = "input_view_stack"
+        public static let textFieldContainer = "input_view_text_field_container"
+    }
+    
+    public class ImageView {
+        public static let id = "image_view"
+        public static let imageView = "image_view_image_view"
+        public static let titleLabel = "image_view_title_label"
+    }
+    
+    public class ButtonView {
+        public static let id = "button_view"
+        public static let label = "button_label"
+    }
+    
+    public class ButtonIconView {
+        public static let id = "button_icon_view"
+        public static let iconView = "button_icon_view_icon_view"
+    }
+    
+    public class TitleView {
+        public static let id = "title_view"
+        public static let label = "title_view_label"
+    }
+    
+    public class ToggleView {
+        public static let id = "toggle_view"
+        public static let switchView = "toggle_view_switch_view"
+    }
+    
+    public class InputOTPItemView {
+        public static let id = "input_otp_item_view_"
+        public static let label = "input_otp_item_view_label"
+    }
+    
+    public class InputOTPView {
+        public static let id = "input_otp_view"
+        public static let itemsStack = "input_otp_items_stack"
+    }
+    
+    public class InputPinItemView {
+        public static let id = "input_pin_item_view_"
+    }
+
+    public class InputPinView {
+        public static let id = "input_pin_view"
+        public static let itemsStack = "input_pin_items_stack"
+    }
+
+    public class KeyboardPinView {
+        public static let id = "keyboard_pin_view"
+        public static let vStack = "keyboard_pin_stack"
+        public static let firstRowStack = "keyboard_pin_first_row_stack"
+        public static let secondRowStack = "keyboard_pin_second_row_stack"
+        public static let thirdRowStack = "keyboard_pin_third_row_stack"
+        public static let fourthRowStack = "keyboard_pin_fourth_row_stack"
+        public static let wrapper = "keyboard_pin_wrapper"
+        public static let key = "keyboard_pin_key_"
+        public static let forgotPin = "keyboard_pin_forgot_pin"
+    }
+    
+    public class KeyPinView {
+        public static let id = "key_pin_view_"
+        
+    }
+    
+    public class KeyPinBottomRightView {
+        public static let id = "key_pin_bottom_right_view"
+        public static let image = "key_pin_bottom_right_image"
+    }
+    
+    public class OldHintView {
+        public static let id = "old_hint_view"
+        public static let hStack = "old_hint_stack"
+        public static let leftLabel = "old_hint_left_label"
+        public static let rightLabel = "old_hint_right_label"
+    }
+    
+    public class RadioView {
+        public static let id = "radio_view"
+        public static let indicatorView = "radio_view_indicator_view"
+    }
+    
+    public class RowView {
+        public static let rowId = "rows_row_"
+        public static let rowCell = "row_cell_"
+        public static let rowCellContainer = "row_cell_container"
+        public static let verticalStack = "vertical_stack"
+        public static let verticalArrayStack = "vertical_array_stack"
+        public static let horizontalStack = "horizontal_stack"
+        public static let horizontalArrayStack = "horizontal_array_stack"
+        public static let horizontalScrollStack = "horizonta_scroll_stack"
+    }
+    
+    public class RowAtoms {
+        public static let title = "row_atoms_title"
+        public static let subtitle = "row_atoms_subtitle"
+        public static let index = "row_atoms_index"
+        public static let amountText = "row_atoms_amount_text"
+        public static let label = "row_atoms_label"
+        public static let image40 = "row_atoms_image40"
+        public static let icon24 = "row_atoms_icon24"
+        public static let icon20 = "row_atoms_icon20"
+        public static let card = "row_atoms_card" // Deprecated
+        public static let toggle = "row_atoms_toggle"
+        public static let checkbox = "row_atoms_checkbox"
+        public static let radio = "row_atoms_radio"
+        public static let button = "row_atoms_button"
+        public static let buttonIcon = "row_atoms_button_icon"
+        public static let titleView = "row_atoms_title_view"
+        public static let cardWithMaskedNumber = "row_atoms_card_with_masked_number"
+        public static let inputView = "row_atoms_input_view"
+        public static let view = "row_atoms_view"
+    }
+}
---
+++ ios-ui-tests-accessibilityids/Sources/AccessibilityIds/ReusableAccessibilityIDs/ReusableAccessibilityIDs.swift
@@ -0,0 +1,61 @@
+//
+//  ReusableAccessibilityIDs.swift
+//
+//
+//  Created by user on 21.06.2024.
+//
+
+public class ReusableAccessibilityIDs {
+    /// Экран запуска
+    public class LaunchScreen {
+        public static let id = "launch_screen"
+    }
+    
+    /// Главный экран
+    public class StocksElements {
+        public static let stockCell = "stock_cell_"
+        public static let stockCellLogoImage = "stock_cell_logo_image"
+        public static let stockCellNameLabel = "stock_cell_name_label"
+        public static let stockCellClasscodeLabel = "stock_cell_classcode_label"
+        public static let stockCellPriceLabel = "stock_cell_price_label"
+        
+        @available(*, deprecated, message: "Use priceDynamicsLabelGreen / priceDynamicsLabelRed instead")
+        public static let stockCellPriceDynamicsLabel = "stock_cell_price_dynamics_label"
+        
+        public static let priceDynamicsLabelGreen = "stock_cell_price_dynamics_label_green"
+        public static let priceDynamicsLabelRed = "stock_cell_price_dynamics_label_red"
+        
+        public static let stockUpdateCell = "stock_update_date_cell"
+        public static let stockUpdateCellInsetsView = "stock_update_date_cell_insets_view"
+        public static let stockUpdateCellDateIcon = "stock_update_date_cell_date_icon"
+        public static let stockUpdateCellDateLabel = "stock_update_date_cell_date_label"
+        
+        public static let stockFilterCell = "stock_filter_cell"
+        public static let stockFilterFirstButtonSelected = "stock_filter_first_button_selected"
+        public static let stockFilterFirstButtonUnselected = "stock_filter_first_button_unselected"
+        public static let stockFilterSecondButtonSelected = "stock_filter_second_button_selected"
+        public static let stockFilterSecondButtonUnselected = "stock_filter_second_button_unselected"
+        public static let stockFilterThirdButtonSelected = "stock_filter_third_button_selected"
+        public static let stockFilterThirdButtonUnselected = "stock_filter_third_button_unselected"
+    }
+    
+    ///  Алерт
+    public class AlertService {
+        public static let subview = "alert_subview"
+    }
+    
+    /// Экран запуска
+    public class AuthService {
+        public static let loginPasswordButton = "login_password_button"
+        public static let loginCachedButton = "login_cached_button"
+        
+        public static let loginButton = "login_button"
+        
+        public static let otpTextField = "otp_field"
+        public static let confirmOtpButton = "confirm_otp_button"
+        
+        public static let pinVC = "pin_view_controller"
+        public static let pinTextField = "pin_field"
+        public static let createPinButton = "create_pin_button"
+    }
+}
